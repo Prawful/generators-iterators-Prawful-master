07c0f99e0aa9268030b762b4856cebe4fed17977
Prawful <prafulla.maharjan@snhu.edu>
2021-04-06 10:49:21 -0400

    Initial commit

---
+++ fibonacciG.py
@@ -0,0 +1,55 @@
+#Define the function fib
+def fib(n):
+#Return 1 when n is 0 or 1
+    if(n == 0):
+        return 1
+
+    elif(n == 1):
+        return 1
+
+#Recursive call to generate fibonacci numbers
+    else:
+        return fib(n-1) + fib(n-2)
+
+#main
+#Generate fibonacci number for n = 1
+x = 1
+print("Fibonacci number for n = 1")
+for i in range(x):
+    print(fib(i))
+
+#Generate fibonacci number for n = 5
+x = 5
+print("Fibonacci number for n = 5")
+for i in range(x):
+    print(fib(i))
+
+#Generate fibonacci number for n = 10
+x = 10
+print("Fibonacci number for n = 10")
+for i in range(x):
+    print(fib(i))
+
+#Generate fibonacci number for n = 20
+x = 20
+print("Fibonacci number for n = 20")
+for i in range(x):
+    print(fib(i))
+
+#Generate fibonacci number for n = 30
+x = 30
+print("Fibonacci number for n = 30")
+for i in range(x):
+    print(fib(i))
+
+#Generate fibonacci number for n = 35
+x = 35
+print("Fibonacci number for n = 35")
+for i in range(x):
+    print(fib(i))
+
+#Generate fibonacci number for n = 40
+x = 40
+print("Fibonacci number for n = 40")
+for i in range(x):
+    print(fib(i))
